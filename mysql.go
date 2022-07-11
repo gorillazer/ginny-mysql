0c46ff58b80e53668d846b7d032e92eeb7b73cfa
linyyyang <linyyyang@tencent.com>
2022-07-11 11:35:30 +0800

    refactor:

--- mysql.go
+++ mysql.go
@@ -14,19 +14,18 @@ import (
 // 默认的keepalive间隔 3h
 const defaultKeepalive = 3 * 60 * 60 * time.Second
 
-// Manager 数据库管理器 读写分离 仅对同一业务库
-type Manager struct {
+// MysqlDB 数据库管理器 读写分离 仅对同一业务库
+type MysqlDB struct {
 	writeDB *sql.DB
 	readDBs []*sql.DB
 
-	ctx    context.Context //控制keepalive goroutine结束
 	cancel context.CancelFunc
 	logger *zap.Logger
 }
 
-// NewManager 根据基础配置 初始化数据库
-func NewManager(config *Config, logger *zap.Logger) (*Manager, error) {
-	writeDB, err := newDB(&config.WDB, config)
+// NewMysqlDB 根据基础配置 初始化数据库
+func NewMysqlDB(ctx context.Context, config *Config, logger *zap.Logger) (*MysqlDB, error) {
+	writeDB, err := newDB(ctx, &config.WDB, config)
 	if err != nil {
 		return nil, err
 	}
@@ -38,45 +37,45 @@ func NewManager(config *Config, logger *zap.Logger) (*Manager, error) {
 			User: config.RDBs[i].User,
 			Pass: config.RDBs[i].Pass,
 		}
-		readDB, err := newDB(source, config)
+		readDB, err := newDB(ctx, source, config)
 		if err != nil {
 			return nil, err
 		}
 		readDBs = append(readDBs, readDB)
 	}
 
-	return NewManagerFromSQLDB(writeDB, readDBs, time.Duration(config.Keepalive)*time.Second, logger), nil
+	return NewMysqlDBFromSqlDB(ctx, writeDB, readDBs,
+		time.Duration(config.Keepalive)*time.Second, logger), nil
 }
 
 // RDB 随机返回一个读库
-func (m *Manager) RDB() *sql.DB {
+func (m *MysqlDB) RDB() *sql.DB {
 	return m.readDBs[rand.Intn(len(m.readDBs))]
 }
 
 // WDB 返回唯一写库
-func (m *Manager) WDB() *sql.DB {
+func (m *MysqlDB) WDB() *sql.DB {
 	return m.writeDB
 }
 
 // Close 关闭所有读写连接池，停止keepalive保活协程。该函数应当很少使用到
-func (m *Manager) Close() {
+func (m *MysqlDB) Close() {
 	m.cancel()
 	if err := m.writeDB.Close(); err != nil {
-		m.logger.Fatal("close db write pool error: ", zap.Error(err))
+		m.logger.With(zap.String("action", "mysql")).Fatal("close write db error", zap.Error(err))
 	}
 	for i := 0; i < len(m.readDBs); i++ {
 		if err := m.readDBs[i].Close(); err != nil {
-			m.logger.Fatal("close db read pool error: ", zap.Error(err))
+			m.logger.With(zap.String("action", "mysql")).Fatal("close db read pool error", zap.Error(err))
 		}
 	}
 }
 
 //
-func newDB(source *Source, config *Config) (*sql.DB, error) {
+func newDB(ctx context.Context, source *Source, config *Config) (*sql.DB, error) {
 	// user:pass@tcp(ip:port)/dbname
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=true&loc=Local&multiStatements=true",
 		source.User, source.Pass, source.Host, config.DBName)
-
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		return nil, err
@@ -87,22 +86,21 @@ func newDB(source *Source, config *Config) (*sql.DB, error) {
 	return db, db.Ping()
 }
 
-// NewManagerFromSQLDB 根据SqlDB对象 初始化数据库
-func NewManagerFromSQLDB(writeDB *sql.DB, readDBs []*sql.DB, keepaliveInterval time.Duration, logger *zap.Logger) *Manager {
+// NewMysqlDBFromSqlDB 根据SqlDB对象 初始化数据库
+func NewMysqlDBFromSqlDB(ctx context.Context, writeDB *sql.DB,
+	readDBs []*sql.DB, keepaliveInterval time.Duration, logger *zap.Logger) *MysqlDB {
 	rand.Seed(time.Now().Unix())
-
-	ctx, cancel := context.WithCancel(context.Background())
-	go keepalive(ctx, writeDB, keepaliveInterval, logger)
+	//控制keepalive goroutine结束
+	c, cancel := context.WithCancel(context.Background())
+	go keepalive(c, writeDB, keepaliveInterval, logger)
 	for i := 0; i < len(readDBs); i++ {
-		go keepalive(ctx, readDBs[i], keepaliveInterval, logger)
+		go keepalive(c, readDBs[i], keepaliveInterval, logger)
 	}
 
-	return &Manager{
+	return &MysqlDB{
 		writeDB: writeDB,
 		readDBs: readDBs,
-		ctx:     ctx,
 		cancel:  cancel,
-		logger:  logger.With(zap.String("type", "DBManager")),
 	}
 }
 
@@ -116,11 +114,11 @@ func keepalive(ctx context.Context, db *sql.DB, interval time.Duration, logger *
 	for {
 		select {
 		case <-ctx.Done():
-			logger.Info("keepalive db end")
+			logger.With(zap.String("action", "mysql")).Info("keepalive db end")
 			return
 		case <-ticker.C:
 			if err := db.Ping(); err != nil {
-				logger.Error("keepalive db ping error", zap.Error(err))
+				logger.With(zap.String("action", "mysql")).Error("keepalive db ping error", zap.Error(err))
 			}
 		}
 	}
